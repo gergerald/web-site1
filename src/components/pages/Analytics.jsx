e907f4223233e6438113f2a0264ce989673fb6c8
MarkRywell <gopro855@gmail.com>
2023-06-24 00:08:23 +0800

    feat: :sparkles: Update page discharged payments

--- src/components/pages/Analytics.jsx
+++ src/components/pages/Analytics.jsx
@@ -1,133 +1,51 @@
-import React, { useState } from 'react';
-import { Container, Typography, TextField, Button, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import React, { useEffect, useState } from "react"
+import {  TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 
 const Analytics = () => {
-  const [formData, setFormData] = useState({});
-  const [submittedData, setSubmittedData] = useState([]);
+  const [patients, setPatients] = useState([]);
 
-  const handleInputChange = (e, field) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [field]: e.target.value,
-    }));
-  };
+  const fetchData = async () => {
+    const response = await fetch("http://127.0.0.1/hospital/discharge/routes.php", {method: "GET"});
+    const data = await response.json();
+    setPatients(data);
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSubmittedData((prevSubmittedData) => [...prevSubmittedData, formData]);
-    setFormData({});
-  };
+  useEffect(() => {
+    fetchData()
+  }, []);
 
   return (
-    <Container maxWidth="sm" sx={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div>
-        <Typography variant="h4" component="h2" sx={{ mt: 2 }}>
-          Discharge
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            label="Patient Number"
-            fullWidth
-            id="pat_no"
-            placeholder="Patient Number"
-            value={formData.pat_no || ''}
-            onChange={(e) => handleInputChange(e, 'pat_no')}
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            label="Discount On"
-            fullWidth
-            id="dis_on"
-            placeholder="Discount On"
-            value={formData.dis_on || ''}
-            onChange={(e) => handleInputChange(e, 'dis_on')}
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            label="Payment Given"
-            fullWidth
-            id="pymt_gv"
-            placeholder="Payment Given"
-            value={formData.pymt_gv || ''}
-            onChange={(e) => handleInputChange(e, 'pymt_gv')}
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            label="Mode of Payment"
-            fullWidth
-            id="mode_of_pymt"
-            placeholder="Mode of Payment"
-            value={formData.mode_of_pymt || ''}
-            onChange={(e) => handleInputChange(e, 'mode_of_pymt')}
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            label="Transaction Given"
-            fullWidth
-            id="tr_gvn"
-            placeholder="Transaction Given"
-            value={formData.tr_gvn || ''}
-            onChange={(e) => handleInputChange(e, 'tr_gvn')}
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            label="Transaction Advised"
-            fullWidth
-            id="tr_advs"
-            placeholder="Transaction Advised"
-            value={formData.tr_advs || ''}
-            onChange={(e) => handleInputChange(e, 'tr_advs')}
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            label="Medicine"
-            fullWidth
-            id="medicine"
-            placeholder="Medicine"
-            value={formData.medicine || ''}
-            onChange={(e) => handleInputChange(e, 'medicine')}
-            sx={{ mt: 3 }}
-          />
-          <Button type="submit" variant="contained" color="primary" sx={{ mt: 3 }}>
-            Submit
-          </Button>
-        </form>
-
-        {submittedData.length > 0 && (
-          <div sx={{ mt: 4 }}>
-            <Typography variant="h5" component="h3">
-              Entered Values:
-            </Typography>
-            <Table sx={{ mt: 2 }}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Patient Number</TableCell>
-                  <TableCell>Discount On</TableCell>
-                  <TableCell>Payment Given</TableCell>
-                  <TableCell>Mode of Payment</TableCell>
-                  <TableCell>Transaction Given</TableCell>
-                  <TableCell>Transaction Advised</TableCell>
-                  <TableCell>Medicine</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {submittedData.map((data, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{data.pat_no}</TableCell>
-                    <TableCell>{data.dis_on}</TableCell>
-                    <TableCell>{data.pymt_gv}</TableCell>
-                    <TableCell>{data.mode_of_pymt}</TableCell>
-                    <TableCell>{data.tr_gvn}</TableCell>
-                    <TableCell>{data.tr_advs}</TableCell>
-                    <TableCell>{data.medicine}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        )}
-      </div>
-    </Container>
+    <div style={{ marginLeft: '240px' }}>
+      <h1>List of Discharged Patients</h1>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Patient Number</TableCell>
+              <TableCell>Dis On</TableCell>
+              <TableCell>Payment Given</TableCell>
+              <TableCell>Mode of Payment</TableCell>
+              <TableCell>Treatment Given</TableCell>
+              <TableCell>Treatment Advice</TableCell>
+              <TableCell>Medicine</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {patients.map((patient) => (
+              <TableRow key={patient.pat_no}>
+                <TableCell>{patient.pat_no}</TableCell>
+                <TableCell>{patient.dis_on}</TableCell>
+                <TableCell>{patient.pymt_gv}</TableCell>
+                <TableCell>{patient.mode_of_pymt}</TableCell>
+                <TableCell>{patient.tr_gvn}</TableCell>
+                <TableCell>{patient.tr_advs}</TableCell>
+                <TableCell>{patient.medicine}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
